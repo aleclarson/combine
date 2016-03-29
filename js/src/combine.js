aa4b86e816854f4779cb625443f6fcddf6db4237
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-03-29 01:05:03 -0700

    03/29/2016

---
+++ js/src/combine.js
@@ -0,0 +1,37 @@
+var combine, isArray, isObject;
+
+isObject = require("isObject");
+
+isArray = Array.isArray;
+
+module.exports = combine = function() {
+  var dest, i, index, j, key, len, len1, source, sources, value;
+  sources = [];
+  for (index = i = 0, len = arguments.length; i < len; index = ++i) {
+    value = arguments[index];
+    sources[index] = value;
+  }
+  dest = sources.shift();
+  if (isArray(sources[0])) {
+    sources = sources[0];
+  }
+  if (!isObject(dest)) {
+    return combine({}, sources);
+  }
+  for (j = 0, len1 = sources.length; j < len1; j++) {
+    source = sources[j];
+    if (!source) {
+      continue;
+    }
+    for (key in source) {
+      value = source[key];
+      if (value === void 0) {
+        continue;
+      }
+      dest[key] = isObject(value) ? combine(dest[key], value) : dest[key] = value;
+    }
+  }
+  return dest;
+};
+
+//# sourceMappingURL=../../map/src/combine.map
--- src/combine.coffee
+++ src/combine.coffee
@@ -1,20 +1,21 @@
 
-isType = (value, type) ->
-  value? and value.constructor is type
-
-isObject = (value) ->
-  (isType value, Object) or (isType value, null)
+isObject = require "isObject"
+isArray = Array.isArray
 
 module.exports =
-combine = (dest, sources...) ->
-  if isType sources[0], Array
+combine = ->
+  sources = [] # Cannot leak arguments object!
+  sources[index] = value for value, index in arguments
+  dest = sources.shift()
+  if isArray sources[0]
     sources = sources[0]
   unless isObject dest
     return combine {}, sources
   for source in sources
+    continue unless source
     for key, value of source
       continue if value is undefined
       dest[key] =
         if isObject value then combine dest[key], value
         else dest[key] = value
-  dest
+  return dest
